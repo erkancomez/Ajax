b733f8197c646b8fb12b716ce40356e4f28b532b
erkancomez <erkancomez@gmail.com>
2020-12-21 21:23:54 +0300

    Add project files.

---
+++ Ajax/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Ajax.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {            
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult List()
+        {
+            Thread.Sleep(3000);
+            var jsonKullanicilar = JsonConvert.SerializeObject(KullaniciIslem.GetAll());
+            return Json(jsonKullanicilar);
+        }
+        public IActionResult GetById(int kullaniciId)
+        {
+            var bulunanKallanici = KullaniciIslem.GetById(kullaniciId);
+            var jsonKullanici = JsonConvert.SerializeObject(bulunanKallanici);
+
+            return Json(jsonKullanici);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpPost]
+        public IActionResult Add(Kullanici kullanici)
+        {
+            KullaniciIslem.Add(kullanici);
+            var jsonKullanci = JsonConvert.SerializeObject(kullanici);
+            return Json(jsonKullanci);
+        }
+    }
+}
---
+++ Ajax/Models/Kullanici.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ajax.Models
+{
+    public class Kullanici
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+    }
+    public class KullaniciIslem
+    {
+        private static readonly List<Kullanici> kullanicilar = new List<Kullanici>
+        { 
+            new Kullanici{Id=1, Ad="kullanici1"},
+            new Kullanici{Id=2, Ad="kullanici2"}
+        };
+        
+        public static List<Kullanici> GetAll()
+        {
+            return kullanicilar;
+        }
+        public static Kullanici GetById(int Id)
+        {
+            return kullanicilar.FirstOrDefault(I => I.Id == Id);
+        }
+        public static void Add(Kullanici kullanici)
+        {
+            kullanicilar.Add(kullanici);
+        }
+
+    }
+}
